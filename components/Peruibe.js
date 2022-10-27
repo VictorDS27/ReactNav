10b74d293758e4454919a0e02857002fa0200701
VictorDS27 <victor.vieira2704@gmail.com>
2022-10-27 09:35:24 -0300

    conhecendo useEffect

--- components/Peruibe.js
+++ components/Peruibe.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import React,{useEffect, useState, } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 
 export default function Peruibe({navigation}) {
+    useEffect(
+        ()=>{
+            function teste(){
+                console.warn("Teste da useEffect");
+            }
+            teste();
+        },[]
+    );
  return (
     <View style={styles.bloco}>
         <Text>Cidade de Peruibe jkkkkkkk</Text>
